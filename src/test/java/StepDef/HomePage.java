3d06fd3f4138b7d942f9fa6565c462eef851dc12
Suraj Patil <prafpawar11@gmail.com>
2023-03-24 08:44:50 +0530

    Home Page Test cases Added

---
+++ src/test/java/StepDef/HomePage.java
@@ -0,0 +1,15 @@
+package StepDef;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Home Page Test case for Logo");
+
+		System.out.println("Home Page Test case for Title");
+
+		System.out.println("Home Page Test case for URL");
+
+	}
+
+}
